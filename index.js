5eb11914d84812dba1f5bbd73d414bae7cd99db9
stephanrotolante <stephanrotolante@gmail.com>
2022-10-21 02:18:22 -0400

    upd yml

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ function run() {
       );
       const params = {
         Bucket: BUCKET,
-        ACL: 'public-read',
         Body: fileStream,
         Key: bucketPath,
         ContentType: lookup(p.path) || 'text/plain'
